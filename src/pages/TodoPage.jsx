1c638ff1e3116b7fa0dae1e047cf7df621e9ec24
evojroan <roan6903@gmail.com>
2023-03-15 23:19:02 +0800

    Todolist connected to API yet unable to show up page content

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,69 +1,73 @@
 import { Footer, Header, TodoCollection, TodoInput } from 'components';
 import { useState } from 'react';
-
-const dummyTodos = [
-  {
-    title: 'Learn react-router',
-    isDone: true,
-    id: 1,
-  },
-  {
-    title: 'Learn to create custom hooks',
-    isDone: false,
-    id: 2,
-  },
-  {
-    title: 'Learn to use context',
-    isDone: true,
-    id: 3,
-  },
-  {
-    title: 'Learn to implement auth',
-    isDone: false,
-    id: 4,
-  },
-];
+import { useEffect } from 'react';
+import { getTodos, createTodo } from 'api/todos';
 
 const TodoPage = () => {
   const [inputValue, setInputValue] = useState('');
-  const [todos, setTodos] = useState(dummyTodos);
+
+  const [todos, setTodos] = useState([]);
+
+  const todoNums = todos.length;
 
   const handleChange = (value) => {
     setInputValue(value);
   };
-
-  const handleTodo = () => {
+  const handleAddTodo = async () => {
     if (inputValue.length === 0) {
       return;
     }
-    setTodos((prevTodos) => {
-      return [
-        ...prevTodos,
-        {
-          id: Math.random() * 100,
-          title: inputValue,
-          isDone: false,
-        },
-      ];
-    });
-    setInputValue('');
+
+    try {
+      const data = await createTodo({
+        title: inputValue,
+        isDone: false,
+      });
+
+      setTodos((prevTodos) => {
+        return [
+          ...prevTodos,
+          {
+            id: data.id,
+            title: data.title,
+            isDone: data.isDone,
+            isEdit: false,
+          },
+        ];
+      });
+
+      setInputValue('');
+    } catch (error) {
+      console.error(error);
+    }
   };
-
-  const handleKeyDown = () => {
+  const handleKeyDown = async () => {
     if (inputValue.length === 0) {
       return;
     }
-    setTodos((prevTodos) => {
-      return [
-        ...prevTodos,
-        {
-          id: Math.random() * 100,
-          title: inputValue,
-          isDone: false,
-        },
-      ];
-    });
-    setInputValue('');
+
+    try {
+      const data = await createTodo({
+        title: inputValue,
+        isDone: false,
+      });
+
+      setTodos((prevTodos) => {
+        return [
+          ...prevTodos,
+          {
+            id: data.id,
+            title: data.title,
+            isDone: data.isDone,
+            isEdit: false,
+          },
+        ];
+      });
+
+      setInputValue('');
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handleToggleDone = (id) => {
@@ -79,7 +83,6 @@ const TodoPage = () => {
       });
     });
   };
-
   const handleChangeMode = ({ id, isEdit }) => {
     setTodos((prevTodos) => {
       return prevTodos.map((todo) => {
@@ -104,38 +107,42 @@ const TodoPage = () => {
             isEdit: false,
           };
         }
-
         return todo;
       });
     });
   };
-
   const handleDelete = (id) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== id);
-    });
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   };
+  useEffect(() => {
+    const getTodosAsync = async () => {
+      try {
+        const todos = await getTodos();
 
-  const totalTodos = todos.length;
-
+        setTodos(todos.map((todo) => ({ ...todo, isEdit: false })));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getTodosAsync();
+  }, []);
   return (
     <div>
-      TodoPage
       <Header />
       <TodoInput
         inputValue={inputValue}
         onChange={handleChange}
-        onAddTodo={handleTodo}
+        onAddTodo={handleAddTodo}
         onKeyDown={handleKeyDown}
       />
       <TodoCollection
         todos={todos}
-        onToggleDone={handleToggleDone}
         onSave={handleSave}
+        onToggleDone={handleToggleDone}
         onChangeMode={handleChangeMode}
         onDelete={handleDelete}
       />
-      <Footer totalTodos={totalTodos} />
+      <Footer numOfTodos={todoNums} />
     </div>
   );
 };
